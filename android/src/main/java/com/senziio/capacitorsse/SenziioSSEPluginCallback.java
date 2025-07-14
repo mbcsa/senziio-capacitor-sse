ba7f842081eef0d1c59831cd46210d02bb0dd703
Ignacio Hurtado <ignacio.hurtado@corsisa.com.ar>
2025-07-14 10:09:05 -0300

    support multiple callback-oriented connections

---
+++ android/src/main/java/com/senziio/capacitorsse/SenziioSSEPluginCallback.java
@@ -0,0 +1,83 @@
+package com.senziio.capacitorsse;
+
+import androidx.annotation.NonNull;
+
+import com.getcapacitor.Bridge;
+import com.getcapacitor.JSObject;
+import com.getcapacitor.PluginCall;
+
+import okhttp3.Response;
+import okhttp3.sse.EventSource;
+import okhttp3.sse.EventSourceListener;
+
+public class SenziioSSEPluginCallback extends EventSourceListener {
+
+    private final PluginCall call;
+
+    private final Bridge bridge;
+
+    public SenziioSSEPluginCallback(PluginCall call, Bridge bridge) {
+        this.call = call;
+        this.bridge = bridge;
+    }
+
+    public String getId() {
+        return call.getCallbackId();
+    }
+
+    @Override
+    public void onOpen(@NonNull EventSource eventSource, @NonNull Response response) {
+        call.resolve(status("connected"));
+    }
+
+    @Override
+    public void onEvent(@NonNull EventSource eventSource, String id, String type, @NonNull String data) {
+        call.resolve(message(type, data));
+    }
+
+    @Override
+    public void onFailure(@NonNull EventSource eventSource, Throwable t, Response response) {
+        eventSource.cancel();
+
+        if (t instanceof Exception ex) {
+            call.reject(ex.getMessage(), ex);
+            return;
+        }
+
+        call.reject("Unknown error", error(t));
+    }
+
+    @Override
+    public void onClosed(@NonNull EventSource eventSource) {
+        call.resolve(status("disconnected"));
+        call.release(bridge);
+    }
+
+    @NonNull
+    private static JSObject status(String s) {
+        JSObject obj = new JSObject();
+        obj.put("type", "status");
+        obj.put("status", s);
+        return obj;
+    }
+
+    @NonNull
+    private static JSObject message(String type, String data) {
+        JSObject obj = new JSObject();
+        obj.put("type", "message");
+
+        JSObject payload = new JSObject();
+        payload.put("type", type != null ? type : "message"); // Asegurar que el tipo no sea nulo
+        payload.put("data", data);
+        obj.put("payload", payload);
+
+        return obj;
+    }
+
+    @NonNull
+    private static JSObject error(Throwable t) {
+        JSObject obj = new JSObject();
+        obj.put("message", t.getMessage());
+        return obj;
+    }
+}
--- src/web.ts
+++ src/web.ts
@@ -1,104 +1,49 @@
 // web.ts
-import { PluginListenerHandle, WebPlugin, ListenerCallback } from '@capacitor/core';
-import {
-  SenziioSSEPlugin,
-  EventType,
-  EventDataMap,
-  eventNames
-} from './definitions';
+import { WebPlugin } from '@capacitor/core';
+import { ConnectionEventCallback, ConnectionID, SenziioSSEPlugin } from './definitions';
 
 export class SenziioSSEWeb extends WebPlugin implements SenziioSSEPlugin {
-  private eventSource: EventSource | null = null;
-  listeners: { [eventName: string]: ListenerCallback[] } = {}; // Ajustamos la definición
+  private readonly connections = new Map<ConnectionID, EventSource>();
+  private sequence = 1;
 
-  async connect(options: { url: string }): Promise<void> {
-    try {
-      this.eventSource = new EventSource(options.url);
+  async connect(options: { url: string }, callback: ConnectionEventCallback): Promise<ConnectionID> {
+    const connectionId = String(this.sequence++);
 
-      this.eventSource.onmessage = (event) => {
-        try {
-          const parsedData = JSON.parse(event.data);
-          this.handleEvent(parsedData.type as EventType, parsedData.data);
-        } catch (error) {
-          console.error('Error parsing event:', error);
-          this.handleEvent('error', { message: 'Error parsing event data' });
-        }
-      };
+    const eventSource = new EventSource(options.url);
 
-      this.eventSource.onopen = () => {
-        this.handleEvent('connected', { status: 'connected' });
-      };
+    eventSource.onmessage = async (event) => {
+      let payload: any;
 
-      this.eventSource.onerror = (error) => {
-        console.error('SSE Error:', error);
-        this.handleEvent('connection_error', { 
-          message: 'SSE connection error',
-          error: error 
-        });
-        this.handleEvent('disconnected', { 
-          status: 'disconnected',
-          reason: 'error' 
-        });
-        this.disconnect();
-      };
-
-    } catch (error) {
-      console.error('Connection setup error:', error);
-      this.handleEvent('connection_error', { 
-        message: 'Failed to setup SSE connection',
-        error: error 
-      });
-      throw error;
-    }
-  }
-
-  private handleEvent<T extends EventType>(type: T, data: EventDataMap[T]) {
-    const eventListeners = this.listeners[type] as ((event: EventDataMap[T]) => void)[] | undefined;
-    if (eventListeners) {
-      eventListeners.forEach(listener => listener(data));
-    }
-  }
-
-  async addListener<T extends EventType>(
-    eventName: T,
-    listenerFunc: (event: EventDataMap[T]) => void
-  ): Promise<PluginListenerHandle> {
-    if (!eventNames.includes(eventName)) {
-      throw new Error(`Evento no válido: ${eventName}`);
-    }
-
-    const typedListenerFunc: ListenerCallback = (data: any) => {
-      listenerFunc(data as EventDataMap[T]);
-    };
-
-    if (!this.listeners[eventName]) {
-      this.listeners[eventName] = [];
-    }
-
-    this.listeners[eventName].push(typedListenerFunc);
-
-    return {
-      remove: async () => {
-        this.listeners[eventName] = this.listeners[eventName]?.filter(
-          listener => listener !== typedListenerFunc
-        );
+      try {
+        payload = JSON.parse(event.data);
+      } catch (error) {
+        console.error('Error parsing event:', event);
+        return;
       }
+
+      await callback({ type: 'message', payload });
     };
+
+    eventSource.onopen = async () => {
+      await callback({ type: 'status', status: 'connected' });
+    };
+
+    eventSource.onerror = async (error) => {
+      await callback(undefined, error);
+      await this.disconnect({ connectionId });
+    };
+
+    this.connections.set(connectionId, eventSource);
+
+    return connectionId;
   }
 
-  async disconnect(): Promise<void> {
-    if (this.eventSource) {
-      this.eventSource.close();
-      this.eventSource = null;
+  async disconnect({ connectionId }: { connectionId: ConnectionID }): Promise<void> {
+    if (!this.connections.has(connectionId)) {
+      throw new Error('ID erróneo: ' + connectionId);
     }
-    this.listeners = {};
-  }
 
-  async removeAllListeners(eventName?: EventType): Promise<void> {
-    if (eventName) {
-      delete this.listeners[eventName];
-    } else {
-      this.listeners = {};
-    }
+    const connection = this.connections.get(connectionId)!;
+    connection.close();
   }
 }
